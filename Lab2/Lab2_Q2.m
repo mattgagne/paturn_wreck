4f77218e89380d485aaec24b12f405a3c6075947
HillaryNgai <hillaryngai29@gmail.com>
2018-03-22 14:37:05 -0400

    Added Uniform Graphs for Question2 - Finished

--- Lab2/Lab2_Q2.m
+++ Lab2/Lab2_Q2.m
@@ -5,19 +5,18 @@
 %Nick Heersink - 20521906 
 
 %% Question 2 - Model Estimation 1-D Case
-%% a)ML Parametric Estimation - Gaussian
 close all; clear all; clc;
 load('lab2_1.mat');
 mu_A = 5;
 sd_A = 1;
 
-max_val_a = max(a(1,:))+1;
-min_val_a = min(a(1,:))-1;
-max_val_b = max(b(1,:))+1;
-min_val_b = min(b(1,:))-1;
+max_val_A = max(a(1,:))+1;
+min_val_A = min(a(1,:))-1;
+max_val_B = max(b(1,:))+1;
+min_val_B = min(b(1,:))-1;
 
 % Gaussian Estimation - Data Set A
-x = 0:0.01:max_val_a;
+x = 0:0.01:max_val_A;
 y = zeros(size(a));
 [muEst, sigmaEst] = gaussianEstimation1D(a);
 
@@ -33,7 +32,7 @@ legend('Samples', 'True Gaussian PDF ', 'Estimated Gaussian PDF');
 xlabel('x'); ylabel('p(x)');
 
 % Gaussian Estimation - Data Set B
-x = 0:0.01:max_val_b;
+x = 0:0.01:max_val_B;
 y = zeros(size(b));
 [muEst, sigmaEst] = gaussianEstimation1D(b);
 
@@ -52,7 +51,7 @@ ylabel('p(x)');
 grid on;
 
 % Exponential Estimation - Data Set A
-x = 0:0.01:max_val_a;
+x = 0:0.01:max_val_A;
 y = zeros(size(a));
 lam = exponentialEstimation1D(a);
 
@@ -68,7 +67,7 @@ legend('Samples', 'True Gaussian PDF ', 'Estimated Exponential PDF');
 xlabel('x'); ylabel('p(x)');
 
 % Exponential Estimation - Data Set B
-x = 0:0.01:max_val_b;
+x = 0:0.01:max_val_B;
 y = zeros(size(b));
 lam = exponentialEstimation1D(b);
 
@@ -83,36 +82,76 @@ title('Exponential Estimation of Exponential Data Set B');
 legend('Samples', 'True Exponential PDF ', 'Estimated Exponential PDF');
 xlabel('x'); ylabel('p(x)');
 
+% Uniform Estimation - Data Set A
+y = zeros(size(a));
+
+x = 0:0.01:max_val_A;
+pdf_gauss = normpdf(x,mu_A, sd_A);
+
+[a_est_A, b_est_A] = uniform_estimation_1D(a);
+uniform_A = unifpdf(x, a_est_A, b_est_A);
+
+figure(5);
+hold on;
+scatter(a, y);
+plot(x, pdf_gauss);
+plot(x,uniform_A);
+title('Uniform Estimation of Gaussian Data Set A');
+legend('Samples', 'True Gaussian PDF ', 'Estimated Uniform PDF');
+xlabel('x');
+ylabel('p(x)');
+grid on;
+
+% Uniform Estimation - Data Set B
+y = zeros(size(b));
+
+x = 0:0.01:max_val_B;
+pdf_exp = exp(-x);
+
+[a_est_B, b_est_B] = uniform_estimation_1D(b);
+uniform_B = unifpdf(x, a_est_B, b_est_B);
+
+figure(6);
+hold on;
+scatter(b, y);
+plot(x, pdf_exp);
+plot(x,uniform_B);
+title('Uniform Estimation of Exponential Data Set B');
+legend('Samples', 'True Exponential PDF ', 'Estimated Uniform PDF');
+xlabel('x');
+ylabel('p(x)');
+grid on;
+
 % Parzen Method with Gaussian Windows using sd = 0.1 and sd = 0.4
 % Non-Parametric Estimation - Data Set A
-x = min_val_a:0.01:max_val_a;
+x = min_val_A:0.01:max_val_A;
 y = zeros(size(a));
 
 pdf_gauss = normpdf(x, mu_A, sd_A);
 p_hat_1 = parzen_estimation_1D(a, x, 0.1);
 p_hat_2 = parzen_estimation_1D(a, x, 0.4);
 
-figure(5);
+figure(7);
 hold on;
 scatter(a, y);
 plot(x, pdf_gauss);
 plot(x, p_hat_1);
 plot(x, p_hat_2);
-title('Parzen Estimation of Exponential Data Set A');
+title('Parzen Estimation of Gaussian Data Set A');
 legend('Samples', 'True Gaussian PDF ', 'Estimated Parzen PDF Using SD = 0.1', 'Estimated Parzen PDF Using SD = 0.4');
 xlabel('x');
 ylabel('p(x)');
 grid on;
 
 % Non-Parametric Estimation - Data Set B
-x = min_val_b:0.01:max_val_b;
+x = min_val_B:0.01:max_val_B;
 y = zeros(size(b));
 
 pdf_exp = exp(-x);
 p_hat_1 = parzen_estimation_1D(b, x, 0.1);
 p_hat_2 = parzen_estimation_1D(b, x, 0.4);
 
-figure(6);
+figure(8);
 hold on;
 scatter(b, y);
 plot(x, pdf_exp);
